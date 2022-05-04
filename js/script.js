95722eb5c7c8dc39b3cd0fc8b4f20819a0b00f25
martemo <marte.moslet@hotmail.com>
2022-05-04 13:59:31 +0200

    update html and js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const url = "https://www.anapioficeandfire.com/api/houses?page=1&pageSize=30"
+const url = "https://www.anapioficeandfire.com/api/houses"
 
 const resultsContainer = document.querySelector(".results");
 
@@ -7,20 +7,25 @@ async function getInfo() {
         const response = await fetch(url);
 
         const results = await response.json();
-    
+
         const houses = results;
 
-        resultsContainer.innerHTML = "";
-    
-        for(let i = 0; i < houses.length; i++) {
-            console.log(houses[i].name);
-            console.log(houses[i].region);
-            console.log(houses[i].coatOfArms);
+        
 
-        resultsContainer.innerHTML += `<div class="result">House: ${houses[i].name}<br>Region: ${houses[i].region}<br>Coat of arms: ${houses[i].coatOfArms}</div>`;
+        resultsContainer.innerHTML = "";
+
+        for(let i = 0; i < houses.length; i ++) {
+            console.log(houses[i].name);
+
+            if(i===3) {
+                break;
+            }
+
+            resultsContainer.innerHTML += `<div class="result"><a href="details.html">${houses[i].name}</a></div>`;
         }
     }
-    catch(error) {
+    
+   catch(error) {
         console.log("An error occorred");
         resultsContainer.innerHTML = displayError("An error occurred when calling the API");
     }   
