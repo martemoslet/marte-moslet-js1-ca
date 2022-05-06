44b81af973054e6feea6be24a3def1be5893111b
martemo <marte.moslet@hotmail.com>
2022-05-06 02:06:27 +0200

    updated contact page

---
+++ js/contact.js
@@ -0,0 +1,53 @@
+const form = document.querySelector("#contactForm");
+const firstName = document.querySelector("#firstName");
+const firstNameError = document.querySelector("#firstNameError");
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const address = document.querySelector("#address");
+const addressError = document.querySelector("#addressError");
+
+function validateForm () {
+    event.preventDefault();
+
+    if (checkLength(firstName.value, 0) === true) {
+        firstNameError.style.display = "none";
+    } else {
+        firstNameError.style.display = "block";
+    }
+
+    if (checkLength(subject.value, 10) === true) {
+        subjectError.style.display = "none";
+    } else {
+        subjectError.style.display = "block";
+    }
+
+    if (checkLength(address.value, 25) === true) {
+        addressError.style.display = "none";
+    } else {
+        addressError.style.display = "block";
+    }
+
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+    }
+}
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
