e3138e549bd52db4eb0a112ab3562a1bd9849bba
martemo <marte.moslet@hotmail.com>
2022-05-07 12:31:26 +0200

    update title

--- js/details.js
+++ js/details.js
@@ -1,5 +1,6 @@
 const details = document.querySelector(".details");
 const queryString = document.location.search;
+const title = document.querySelector(".title")
 
 // create an object that will allows us to access all the query string parameters
 const params = new URLSearchParams(queryString);
@@ -7,14 +8,12 @@ const params = new URLSearchParams(queryString);
 // get the id parameter from the query string
 const id = params.get("id");
 
-console.log("The id is " + id);
-
 async function getData(houseId) {
     try {
         const response = await fetch("https://wizard-world-api.herokuapp.com/Houses/" + houseId);
         const results = await response.json();
         details.innerHTML = "";
-        console.log(results);
+        title.innerHTML = "";
 
         details.innerHTML = `<h1>${results.name}</h1>
                             <ul>
@@ -24,6 +23,7 @@ async function getData(houseId) {
                             <li>Ghost: ${results.ghost}</li>
                             <li>Common room: ${results.commonRoom}</li>
                             </ul>`
+        title.innerHTML = `${results.name}`;
     }
     
    catch(error) {
